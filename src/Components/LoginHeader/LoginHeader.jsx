7bee6be6f1d02124fad6b64a1df05f96fa25da51
unknown <RHAYSSA0483@digitalcollegesul.local>
2024-07-19 12:17:49 -0300

    ajustando header

--- src/Components/LoginHeader/LoginHeader.jsx
+++ src/Components/LoginHeader/LoginHeader.jsx
@@ -1,19 +1,19 @@
 import DigitalStore from "../../assets/DigitalStore.png"
 // import Search from "../../assets/Search.png"
-import Buy from "../../assets/Buy.png"
+// import Buy from "../../assets/Buy.png"
 import "./LoginHeader.css"
 
 function LoginHeader() {
 
     return (
         <>
-            <nav className="header">
+            <header className="header">
                 <img id="img1" src={DigitalStore} alt="" />
                     <input type="text" placeholder="Pesquisar produto..."></input>
                 <a id="cadastro" href=""><u>Cadastre-se</u></a>
-                <button>Entrar</button>
-                <button><img src={Buy}/></button>
-            </nav>
+                <a id="entrar" href="">Entrar</a>
+                <a id="carrinho" href=""></a>
+            </header>
 
         </>
     )
